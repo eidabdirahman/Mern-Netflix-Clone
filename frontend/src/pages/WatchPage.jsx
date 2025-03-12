d907c60f017222fe680284f52193a82fe181b6e3
Eid Abdirahman <140173086+eidabdirahman@users.noreply.github.com>
2025-03-12 08:36:37 +0300

    .

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { useContentStore } from "../store/content";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import { ChevronLeft, ChevronRight } from "lucide-react";
@@ -8,6 +7,7 @@ import ReactPlayer from "react-player";
 import { ORIGINAL_BASE_IMAGE_URL, SMALL_BASE_IMAGE_URL } from "../utils/constants";
 import { formatReleaseDate } from "../utils/dateFunction";
 import WatchPageSkeleton from "../components/skeletons/WatchPageSkeleton";
+import { useContentStore } from "../store/content";
 
 const WatchPage = () => {
 	const { id } = useParams();
