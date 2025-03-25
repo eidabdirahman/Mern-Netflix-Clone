a73dac582da78ed00c827d8c5748884e0bea71b5
Eid Abdirahman <eid@Eid.Eid>
2025-03-25 15:25:14 +0300

    ..

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,7 @@ app.use("/api/v1/movie", ProtectRoute, MovieRoutes);
 app.use("/api/v1/tv", ProtectRoute, tvRoutes);
 app.use("/api/v1/search", ProtectRoute, SearchRoutes);
 
-if (envVars.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-}
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
 
 
 app.listen(port, () => {
